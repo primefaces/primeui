01a97c1fd58affa4bcb1a39da22be73d5efbf86a
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2013-08-06 09:56:26 +0000

    Drop in of picklist widget with core functionalities

---
+++ src/main/webapp/primeui/js/picklist/picklist.js
@@ -0,0 +1,229 @@
+/**
+ * PrimeUI picklist widget
+ */
+$(function() {
+
+    $.widget("primeui.puipicklist", {
+       
+        options: {
+            effect: 'fade',
+            effectSpeed: 'fast'
+        },
+
+        _create: function() {
+            this.element.addClass('pui-picklist ui-widget ui-helper-clearfix');
+            this.inputs = this.element.children('select');
+            this.items = $();
+            this.sourceInput = this.inputs.eq(0);
+            this.targetInput = this.inputs.eq(1);
+            
+            this.sourceList = this._createList(this.sourceInput);
+            this._createButtons();
+            this.targetList = this._createList(this.targetInput);
+            
+            this._bindEvents();
+        },
+                
+        _createList: function(input, cssClass) {
+            input.wrap('<div class="ui-helper-hidden"></div>');
+            
+            var listContainer = $('<ul class="ui-widget-content pui-picklist-list pui-inputtext ui-corner-all ' + cssClass + '"></ul>'),
+            options = input.children('option');
+    
+            for(var i = 0; i < options.length; i++) {
+                var option = options.eq(i),
+                item = $('<li class="pui-picklist-item ui-corner-all">' + option.text() + '</li>').data({
+                    'item-label': option.text(),
+                    'item-value': option.val()
+                });
+        
+                this.items = this.items.add(item);
+                listContainer.append(item);
+            }
+            
+            this.element.append(listContainer);
+            
+            return listContainer;
+        },
+                
+        _createButtons: function() {
+            var $this = this,
+            buttonContainer = $('<ul class="pui-picklist-buttons"></ul>')
+            
+            buttonContainer.append(this._createButton('ui-icon-arrow-1-e', 'pui-picklist-button-add', function(){$this._add();}))
+                            .append(this._createButton('ui-icon-arrowstop-1-e', 'pui-picklist-button-addall', function(){$this._addAll();}))
+                            .append(this._createButton('ui-icon-arrow-1-w', 'pui-picklist-button-remove', function(){$this._remove();}))
+                            .append(this._createButton('ui-icon-arrowstop-1-w', 'pui-picklist-button-removeall', function(){$this._removeAll();}));
+                    
+            this.element.append(buttonContainer);
+        },
+                
+        _createButton: function(icon, cssClass, fn) {
+            var btn = $('<button class="' + cssClass + '" type="button"></button>').puibutton({
+                'icon': icon,
+                'click': fn
+            });
+            
+            return btn;
+        },
+                
+        _bindEvents: function() {
+            var $this = this;
+        
+            this.items.on('mouseover.puipicklist', function(e) {
+                var element = $(this);
+
+                if(!element.hasClass('ui-state-highlight')) {
+                    $(this).addClass('ui-state-hover');
+                }
+            })
+            .on('mouseout.puipicklist', function(e) {
+                $(this).removeClass('ui-state-hover');
+            })
+            .on('click.puipicklist', function(e) {
+                var item = $(this),
+                metaKey = (e.metaKey||e.ctrlKey);
+
+                if(!e.shiftKey) {
+                    if(!metaKey) {
+                        $this.unselectAll();
+                    }
+
+                    if(metaKey && item.hasClass('ui-state-highlight')) {
+                        $this.unselectItem(item);
+                    } 
+                    else {
+                        $this.selectItem(item);
+                        $this.cursorItem = item;
+                    }
+                }
+                else {
+                    $this.unselectAll();
+
+                    if($this.cursorItem && ($this.cursorItem.parent().is(item.parent()))) {
+                        var currentItemIndex = item.index(),
+                        cursorItemIndex = $this.cursorItem.index(),
+                        startIndex = (currentItemIndex > cursorItemIndex) ? cursorItemIndex : currentItemIndex,
+                        endIndex = (currentItemIndex > cursorItemIndex) ? (currentItemIndex + 1) : (cursorItemIndex + 1),
+                        parentList = item.parent();
+
+                        for(var i = startIndex ; i < endIndex; i++) {
+                            $this.selectItem(parentList.children('li.ui-picklist-item').eq(i));
+                        }
+                    } 
+                    else {
+                        $this.selectItem(item);
+                        $this.cursorItem = item;
+                    }
+                }
+            })
+            .on('dblclick.pickList', function() {
+                var item = $(this);
+
+                if($(this).parent().hasClass('ui-picklist-source'))
+                    $this.transfer(item, $this.sourceList, $this.targetList, 'dblclick');
+                else
+                    $this.transfer(item, $this.targetList, $this.sourceList, 'dblclick');
+
+                PUI.clearSelection();
+            });
+        },
+                
+        selectItem: function(item) {
+            item.removeClass('ui-state-hover').addClass('ui-state-highlight');
+        },
+
+        unselectItem: function(item) {
+            item.removeClass('ui-state-highlight');
+        },
+
+        unselectAll: function() {
+            var selectedItems = this.items.filter('.ui-state-highlight');
+            for(var i = 0; i < selectedItems.length; i++) {
+                this.unselectItem(selectedItems.eq(i));
+            }
+        },
+                
+        _add: function() {
+            var items = this.sourceList.children('li.pui-picklist-item.ui-state-highlight')
+
+            this._transfer(items, this.sourceList, this.targetList, 'command');
+        },
+
+        _addAll: function() {
+            var items = this.sourceList.children('li.pui-picklist-item:visible:not(.ui-state-disabled)');
+
+            this._transfer(items, this.sourceList, this.targetList, 'command');
+        },
+
+        _remove: function() {
+            var items = this.targetList.children('li.pui-picklist-item.ui-state-highlight');
+
+            this._transfer(items, this.targetList, this.sourceList, 'command');
+        },
+
+        _removeAll: function() {
+            var items = this.targetList.children('li.pui-picklist-item:visible:not(.ui-state-disabled)');
+
+            this._transfer(items, this.targetList, this.sourceList, 'command');
+        },
+                
+        _transfer: function(items, from, to, type) {  
+            var $this = this,
+            itemsCount = items.length,
+            transferCount = 0;
+
+            if(this.options.effect) {
+                items.hide(this.options.effect, {}, this.options.effectSpeed, function() {
+                    var item = $(this);
+                    $this.unselectItem(item);
+
+                    item.appendTo(to).show($this.options.effect, {}, $this.options.effectSpeed, function() {
+                        transferCount++;
+
+                        if(transferCount === itemsCount) {
+                            $this._saveState();
+                            $this._triggerTransferEvent(items, from, to, type);
+                        }
+                    });
+                });
+            }
+            else {
+                items.hide().removeClass('ui-state-highlight').appendTo(to).show();
+
+                this._saveState();
+                this._triggerTransferEvent(items, from, to, type);
+            }
+        },
+
+        _triggerTransferEvent: function(items, from, to, type) {
+            var obj = {};
+            obj.items = items;
+            obj.from = from;
+            obj.to = to;
+            obj.type = type;
+
+            this._trigger('transfer', null, obj);
+        },
+                
+        _saveState: function() {
+            this.sourceInput.children().remove();
+            this.targetInput.children().remove();
+
+            this._generateItems(this.sourceList, this.sourceInput);
+            this._generateItems(this.targetList, this.targetInput);
+            this.cursorItem = null;
+        },
+                
+        _generateItems: function(list, input) {   
+            list.children('.pui-picklist-item').each(function() {
+                var item = $(this),
+                itemValue = item.data('item-value'),
+                itemLabel = item.data('item-label');
+
+                input.append('<option value="' + itemValue + '" selected="selected">' + itemLabel + '</option>');
+            });
+        }
+    });
+        
+});
