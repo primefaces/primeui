93d6da43f952df5ab5f9e8811c9090438e4d4c5f
Çağatay Çivici <cagatay.civici@github.com>
2015-06-01 20:55:04 +0300

    Fixes in progress

--- src/main/webapp/resources/js/site.js
+++ src/main/webapp/resources/js/site.js
@@ -1,264 +1,115 @@
 var winW=$(window).width();
 var winH=$(window).height();
 
-$(document).ready(function() {
-    
-    var Showcase = {
-    
-        menu: $('#MENUSIDE'),
-        
-        content: $('#CONTENTSIDE'),
-        
-        categoryLinks: $(".MainLinkText"),
-        
-        menuVisible: false,
-                
-        logo: $('#LOGO'),
-        
-        hiddenMenuIcons: $('.hiddenIcons'),
-        
-        hiddenLogo: $('#BlueLogo'),
-                
-        submenus: $('.SubMenuLinkContainer'),
-        
-        searchInput: $('#menuSearch'),
-        
-        topLinksCover: $('#PFTopLinksCover'),
-        
-        contentSideIndent: $('#CONTENTSIDEindent'),
-                
-        desktopContainer: $(document.body).children('.PC'),
-        
-        mobileContainer: $(document.body).children('.MOBILE'),
-        
-        activeMenu: window.activeMenu,
-        
-        activeSubSubMenu: null,
-
-        highlightMenu: function() {
-            this.clearMenuAnimQueue();
-            this.hiddenMenuIcons.animate({opacity:1}, 250);
-            this.hiddenLogo.animate({opacity:1}, 250);
-        },
-
-        unhighlightMenu: function() {
-            this.clearMenuAnimQueue();
-            this.hiddenMenuIcons.animate({opacity:0}, 250);
-            this.hiddenLogo.animate({opacity:0}, 250);
-        },
-        
-        clearMenuAnimQueue: function() {
+Showcase = {
             
-        },
+    init: function() {
+        this.menu = $('#MENUSIDE');
+        this.hiddenMenuIcons = $('.hiddenIcons');
+        this.hiddenLogo = $('#BlueLogo');
+        this.menu.perfectScrollbar({
+            wheelSpeed: 40,
+            suppressScrollX:true
+        });
+
+        this.bindEvents();
         
-        onWinResize: function() {
-            this.menu.height($(window).height());
-        },
+        this.openHashPage();
+    },
+
+    bindEvents: function() {
+        this.menu.on("mouseenter", function() {
+            Showcase.highlightMenu();
+        })
+        .on("mouseleave", function() {
+            Showcase.unhighlightMenu();
+        });
         
-        hideSubMenus: function() {
-            if(this.activeMenu) {
-                $(this.activeMenu).removeClass('MenuSideMainLinkDark').next().hide();
+        $('#mobilemenu').on('change', function(e) {
+            var url = $(this).val();
+            if(url.length > 0) {
+                window.location = url;
+            }
+        });
+        
+        this.menu.find('a.SubMenuLink').on('click', function(e) {
+            var href = $(this).attr('href');
+            $.get(href, function(content) {
+                $('#widgetdemo').html(content);
+            });
+
+            window.location.hash = href.substring(href.lastIndexOf('/'), href.indexOf('.html'));
+            e.preventDefault();
+        });
+        
+        $(window).on('hashchange', function (e) {
+            //Showcase.openHashPage();
+            e.preventDefault();
+        });
+    },
+
+    highlightMenu: function() {
+        this.hiddenMenuIcons.animate({opacity:1}, 250);
+        this.hiddenLogo.animate({opacity:1}, 250);
+    },
+
+    unhighlightMenu: function() {
+        this.hiddenMenuIcons.animate({opacity:0}, 250);
+        this.hiddenLogo.animate({opacity:0}, 250);
+    },
+
+    openSubMenu: function(header) {
+        var headerJQ = $(header);
+
+        if(this.activeSubSubMenu) {
+            $(this.activeSubSubMenu).removeClass("openSubMenuLink");
+            this.activeSubSubMenu = null;
+        }
+
+        if(this.activeMenu) {
+            if(this.activeMenu === header) {
+                headerJQ.removeClass('MenuSideMainLinkDark').next().slideUp(700,"easeInOutQuint");
                 this.activeMenu = null;
-            }
-            
-            if(this.activeSubSubMenu) {
-                $(this.activeSubSubMenu).removeClass('openSubMenuLink');
-                this.activeSubSubMenu = null;
-            }
-        },
-        
-        openSubMenu: function(header) {
-            var headerJQ = $(header);
-            
-            if(this.activeSubSubMenu) {
-                $(this.activeSubSubMenu).removeClass("openSubMenuLink");
-                this.activeSubSubMenu = null;
-            }
-            
-            if(this.activeMenu) {
-                if(this.activeMenu === header) {
-                    headerJQ.removeClass('MenuSideMainLinkDark').next().slideUp(700,"easeInOutQuint");
-                    this.activeMenu = null;
-                    $.removeCookie('menustate', {path: '/'});
-                }
-                else {
-                    $(this.activeMenu).removeClass('MenuSideMainLinkDark').next().slideUp(700,"easeInOutQuint");
-                    headerJQ.addClass("MenuSideMainLinkDark").next().slideDown(700,"easeInOutQuint");
-                    this.activeMenu = header;
-                    $.cookie('menustate', headerJQ.attr('id'), {path: '/'});
-                }
+                $.removeCookie('menustate', {path: '/'});
             }
             else {
+                $(this.activeMenu).removeClass('MenuSideMainLinkDark').next().slideUp(700,"easeInOutQuint");
                 headerJQ.addClass("MenuSideMainLinkDark").next().slideDown(700,"easeInOutQuint");
                 this.activeMenu = header;
                 $.cookie('menustate', headerJQ.attr('id'), {path: '/'});
             }
-        },
-        
-        openSubSubMenu: function(submenuLink){
-            if(this.activeSubSubMenu) {
-                if(this.activeSubSubMenu !== submenuLink) {
-                    $(this.activeSubSubMenu).removeClass("openSubMenuLink");
-                    $(submenuLink).addClass("openSubMenuLink");
-                    this.activeSubSubMenu = submenuLink;
-                }
-            }
-            else {
-                $(submenuLink).addClass("openSubMenuLink");
-                this.activeSubSubMenu = submenuLink;
-            }
         }
-    };
-	
-	Showcase.onWinResize();
-	$(window).on("resize", function() {
-        Showcase.onWinResize();
-    });
-		
-	Showcase.menu.perfectScrollbar({
-		wheelSpeed: 40,
-		suppressScrollX:true
-	});
-    	
-	// menu mouseenter & mouseleave actions start ----------------------------------
-	Showcase.menu.on("mouseenter", function() {
-        Showcase.highlightMenu();
-	})
-	.on("mouseleave", function() {
-        Showcase.unhighlightMenu();
-	});
-    
-    // open theme switcher combo
-    $("#themeSwitcher").on("click",function(){
-		$("#GlobalThemeSwitcher").slideDown(500);
-	})
-    .on("mouseleave",function(){
-		$("#GlobalThemeSwitcher").slideUp(1);
-	});
-    
-    $("#GlobalThemeSwitcher > a").on("click", function(e) {
-        var theme = $(this).data("theme");
-        changeTheme([{name:'globaltheme', value:theme}]);
-        PrimeFaces.changeTheme(theme);
-        e.preventDefault();
-    });
-    
-    // open theme switcher combo
-    $("#pushNav").on("click",function(){
-		$("#PushDemos").slideDown(500);
-	})
-    .on("mouseleave",function(){
-		$("#PushDemos").slideUp(1);
-	});
-    
-    //mobile menu
-    $('#mobilemenu').on('change', function(e) {
-        var url = $(this).val();
-        if(url.length > 0) {
-            window.location = url;
+        else {
+            headerJQ.addClass("MenuSideMainLinkDark").next().slideDown(700,"easeInOutQuint");
+            this.activeMenu = header;
+            $.cookie('menustate', headerJQ.attr('id'), {path: '/'});
         }
-    });
-    
-    var sourceTabview = $('#SourceContentSide > div > span > span > div.ui-tabs'),
-    lastTabHeader = sourceTabview.find('> ul > li:last');
-    if(lastTabHeader.hasClass('tab-doc')) {
-        lastTabHeader.one('click.load', function() {
-            var classes = lastTabHeader.attr('class').split(' '),
-            slide = 0;
+    },
 
-            for(var i = 0; i < classes.length; i++) {
-                if(classes[i].indexOf('docslide-') === 0) {
-                    slide = classes[i].split('-')[1];
-                }
-            }
+    openHashPage: function() {
+        var hash = window.location.hash,
+        item = null;
 
-            var content = sourceTabview.find('> .ui-tabs-panels > div:last ');
-            content.html('<iframe frameborder="0" class="speakerdeck-iframe" style="border: 0px none; background: none repeat scroll 0% 0% transparent; margin: 0px; padding: 0px; width: 100%; height: 1440px;" src="http://speakerdeck.com/player/057b8d7962d2466f9f24cd45f559b8da?slide=' + slide +'"  allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>');
-        });
-
-    }
-    
-     // Search ---------------------------------------
-    Showcase.searchInput.on('keyup', function(e) {
-        Showcase.hideSubMenus();
-        
-        if (e.keyCode === 32) {
-            $(this).val($(this).val()+" ");
+        if(hash.length) {
+            hash = hash.replace("#","");
+            item = $('#MENUSIDE .SubMenuLink').filter("[href^="+ hash +"]");
+            item.trigger("click");
         }
-        var searchValue = $(this).val().toLowerCase(),
-        matchSub = false;
+     },
 
-        $('.SubMenuLinkContainer').each(function() {
-            var MenuSideValue = $.trim($(this).prev().children('span').text()).toLowerCase(),
-            itemValue;
-            
-            if(MenuSideValue.search(searchValue) < 0 || searchValue.length === 0) {  
-                var Sub = $(this).children('a');
+     restoreMenuState: function() {
+         var activeMenuId = $.cookie('menustate');
+         this.activeMenu = document.getElementById(activeMenuId);
 
-                for(var i = 0; i < Sub.length; i++) {     //for SubMenu
-                    itemValue = $.trim(Sub.eq(i).text()).toLowerCase();
-                    if(itemValue.search(searchValue) >= 0) {
-                        Sub.eq(i).show();
-                        matchSub = true;
-                    }
-                    else{
-                        Sub.eq(i).hide();
-                    }
-                }
-                
-                if(matchSub) {
-                    $(this).prev().show();
-                    matchSub = false;
-                }
-                else {
-                    $(this).prev().hide();
-                }
-            }
-            else {
-                $(this).children().show();
-                $(this).prev().show();
-            }
-        });
-    });
-   
-    $('#MENUSIDE .SubMenuLink').on('click', function(e) {
-       var href = $(this).attr('href');
-       $.get(href, function(content) {
-           $('#widgetdemo').html(content);
-       });
-       
-       window.location.hash = href.split('.')[0]; 
-       e.preventDefault();
-   });
+         if(this.activeMenu) {
+             $(this.activeMenu).addClass('MenuSideMainLinkDark').next('.SubMenuLinkContainer').show();
+         }
+     }
+};
 
-   searchLocationHash();
-   window.Showcase = Showcase;
+$(function() {
+    Showcase.init();
 });
-
-$(window).bind('hashchange', function (e) {
-    searchLocationHash();
-    e.preventDefault();
-});
-
-function searchLocationHash() {
-   var hash = window.location.hash,
-       item = null;
-
-   if(hash.length) {
-       hash = hash.replace("#","");
-       item = $('#MENUSIDE .SubMenuLink').filter("[href^="+ hash +"]");
-       item.trigger("click");
-   }
-}
-
-function restoreMenuState() {
-    var activeMenuId = $.cookie('menustate');
-    window.activeMenu = document.getElementById(activeMenuId);
-    
-    if(window.activeMenu) {
-        $(window.activeMenu).addClass('MenuSideMainLinkDark').next('.SubMenuLinkContainer').show();
-    }
-}
 
 /*!
  * jQuery Cookie Plugin v1.4.1
