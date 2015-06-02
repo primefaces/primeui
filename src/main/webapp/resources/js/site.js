32fe915423280d59b62168d53493b97e51268d0d
Çağatay Çivici <cagatay.civici@github.com>
2015-06-03 01:11:28 +0300

    Refactor

--- src/main/webapp/resources/js/site.js
+++ src/main/webapp/resources/js/site.js
@@ -5,7 +5,7 @@ Showcase = {
             
     init: function() {
         this.menu = $('#MENUSIDE');
-        this.hiddenMenuIcons = $('.hiddenIcons');
+        this.hiddenMenuIcons = this.menu.find('> div > span.MenuSideMainLink > .hiddenIcons');
         this.hiddenLogo = $('#BlueLogo');
         this.menu.perfectScrollbar({
             wheelSpeed: 40,
@@ -78,19 +78,16 @@ Showcase = {
             if(this.activeMenu === header) {
                 headerJQ.removeClass('MenuSideMainLinkDark').next().slideUp(700,"easeInOutQuint");
                 this.activeMenu = null;
-                $.removeCookie('menustate', {path: '/'});
             }
             else {
                 $(this.activeMenu).removeClass('MenuSideMainLinkDark').next().slideUp(700,"easeInOutQuint");
                 headerJQ.addClass("MenuSideMainLinkDark").next().slideDown(700,"easeInOutQuint");
                 this.activeMenu = header;
-                $.cookie('menustate', headerJQ.attr('id'), {path: '/'});
             }
         }
         else {
             headerJQ.addClass("MenuSideMainLinkDark").next().slideDown(700,"easeInOutQuint");
             this.activeMenu = header;
-            $.cookie('menustate', headerJQ.attr('id'), {path: '/'});
         }
     },
     
@@ -108,136 +105,9 @@ Showcase = {
         $.get(url, function(content) {
             $('#widgetdemo').html(content);
         });
-    },
-
-    restoreMenuState: function() {
-         var activeMenuId = $.cookie('menustate');
-         this.activeMenu = document.getElementById(activeMenuId);
-
-         if(this.activeMenu) {
-             $(this.activeMenu).addClass('MenuSideMainLinkDark').next('.SubMenuLinkContainer').show();
-         }
-     }
+    }
 };
 
 $(function() {
     Showcase.init();
 });
-
-/*!
- * jQuery Cookie Plugin v1.4.1
- * https://github.com/carhartl/jquery-cookie
- *
- * Copyright 2013 Klaus Hartl
- * Released under the MIT license
- */
-(function (factory) {
-	if (typeof define === 'function' && define.amd) {
-		// AMD
-		define(['jquery'], factory);
-	} else if (typeof exports === 'object') {
-		// CommonJS
-		factory(require('jquery'));
-	} else {
-		// Browser globals
-		factory(jQuery);
-	}
-}(function ($) {
-
-	var pluses = /\+/g;
-
-	function encode(s) {
-		return config.raw ? s : encodeURIComponent(s);
-	}
-
-	function decode(s) {
-		return config.raw ? s : decodeURIComponent(s);
-	}
-
-	function stringifyCookieValue(value) {
-		return encode(config.json ? JSON.stringify(value) : String(value));
-	}
-
-	function parseCookieValue(s) {
-		if (s.indexOf('"') === 0) {
-			// This is a quoted cookie as according to RFC2068, unescape...
-			s = s.slice(1, -1).replace(/\\"/g, '"').replace(/\\\\/g, '\\');
-		}
-
-		try {
-			// Replace server-side written pluses with spaces.
-			// If we can't decode the cookie, ignore it, it's unusable.
-			// If we can't parse the cookie, ignore it, it's unusable.
-			s = decodeURIComponent(s.replace(pluses, ' '));
-			return config.json ? JSON.parse(s) : s;
-		} catch(e) {}
-	}
-
-	function read(s, converter) {
-		var value = config.raw ? s : parseCookieValue(s);
-		return $.isFunction(converter) ? converter(value) : value;
-	}
-
-	var config = $.cookie = function (key, value, options) {
-
-		// Write
-
-		if (value !== undefined && !$.isFunction(value)) {
-			options = $.extend({}, config.defaults, options);
-
-			if (typeof options.expires === 'number') {
-				var days = options.expires, t = options.expires = new Date();
-				t.setTime(+t + days * 864e+5);
-			}
-
-			return (document.cookie = [
-				encode(key), '=', stringifyCookieValue(value),
-				options.expires ? '; expires=' + options.expires.toUTCString() : '', // use expires attribute, max-age is not supported by IE
-				options.path    ? '; path=' + options.path : '',
-				options.domain  ? '; domain=' + options.domain : '',
-				options.secure  ? '; secure' : ''
-			].join(''));
-		}
-
-		// Read
-
-		var result = key ? undefined : {};
-
-		// To prevent the for loop in the first place assign an empty array
-		// in case there are no cookies at all. Also prevents odd result when
-		// calling $.cookie().
-		var cookies = document.cookie ? document.cookie.split('; ') : [];
-
-		for (var i = 0, l = cookies.length; i < l; i++) {
-			var parts = cookies[i].split('=');
-			var name = decode(parts.shift());
-			var cookie = parts.join('=');
-
-			if (key && key === name) {
-				// If second argument (value) is a function it's a converter...
-				result = read(cookie, value);
-				break;
-			}
-
-			// Prevent storing a cookie that we couldn't decode.
-			if (!key && (cookie = read(cookie)) !== undefined) {
-				result[name] = cookie;
-			}
-		}
-
-		return result;
-	};
-
-	config.defaults = {};
-
-	$.removeCookie = function (key, options) {
-		if ($.cookie(key) === undefined) {
-			return false;
-		}
-
-		// Must not alter options, thus extending a fresh object...
-		$.cookie(key, '', $.extend({}, options, { expires: -1 }));
-		return !$.cookie(key);
-	};
-
-}));
