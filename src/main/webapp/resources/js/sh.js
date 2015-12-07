5bd0723e7fa75677e3f056595cf431e71fbd7a97
mertsincan <sincan.mert@gmail.com>
2015-12-07 12:24:41 +0200

    Refactor

--- src/main/webapp/resources/js/sh.js
+++ src/main/webapp/resources/js/sh.js
@@ -234,6 +234,8 @@ eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a
 }
 else {
     SyntaxHighlighter = {
-        highlight: false
+        highlight: function() {
+            return false;
+        }
     }
 }
