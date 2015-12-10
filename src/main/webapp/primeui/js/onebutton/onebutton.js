b11d25b21129663c7edf257b6c491a901c366a7d
Çağatay Çivici <cagatay.civici@github.com>
2015-12-10 17:08:56 +0200

    Refactored oneButton

--- src/main/webapp/primeui/js/onebutton/onebutton.js
+++ src/main/webapp/primeui/js/onebutton/onebutton.js
@@ -5,27 +5,30 @@
        options: {
             choices: null,
             formfield: null,
-            unselectable: false
+            unselectable: false,
+            tabindex: '0'
         },
         
         _create: function() {
-            this.element.addClass('pui-selectonebutton pui-buttonset ui-widget ui-corner-all pui-buttonset-3');
-            for (var i = 0; i < this.options.choices.length; i++) {
-                var buttonMarkUp = '<div class = "pui-button ui-widget ui-state-default pui-button-text-only " data-value = "'+ this.options.choices[i].value +'">' +
-                                    '<span class = "pui-button-text ui-c">' + this.options.choices[i].label + '</span></div>';
-
-                this.element.append(buttonMarkUp);
-                
+            this.element.addClass('pui-selectonebutton pui-buttonset ui-widget ui-corner-all').attr('tabindex');
+            
+            //create buttons
+            if(this.options.choices) {
+                for(var i = 0; i < this.options.choices.length; i++) {
+                    this.element.append('<div class="pui-button ui-widget ui-state-default pui-button-text-only" tabindex="' + this.options.tabindex + '" data-value="' 
+                                        + this.options.choices[i].value + '">' +
+                                        '<span class="pui-button-text ui-c">' + 
+                                        this.options.choices[i].label + 
+                                        '</span></div>');
+                }
             }
-            var firstChild = $('#xxx .pui-button:first-child');
-            var lastChild = $('#xxx .pui-button:last-child');
-            firstChild.addClass('ui-corner-left');
-            lastChild.addClass('ui-corner-right');
+            
+            //cornering
+            this.buttons = this.element.children('div.pui-button');
+            this.buttons.filter(':first-child').addClass('ui-corner-left');
+            this.buttons.filter(':last-child').addClass('ui-corner-right');
            
-            this.input = $('<input type = "hidden" ></input>').appendTo(this.element);
-            this.element.children().attr('tabindex', '0');
-            this.oneButton = this.element.children(".pui-button");
-            this.optionUnselectable = this.options.unselectable;
+            this.input = $('<input type="hidden"></input>').appendTo(this.element);
             
             this._bindEvents();
         },
@@ -33,23 +36,25 @@
         _bindEvents: function() {
             var $this = this;
             
-            this.element.children().on('mouseover', function() {
+            this.buttons.on('mouseover', function() {
                 var btn = $(this);
                 if(!btn.hasClass('ui-state-active')) {
-                   btn.addClass('ui-state-hover');
+                    btn.addClass('ui-state-hover');
                 }
             })
             .on('mouseout', function() {
                 $(this).removeClass('ui-state-hover');
             })
             .on('click', function() {
-                var isActive = $(this).hasClass("ui-state-active");
+                var btn = $(this);
                 
-                $this.oneButton.removeClass("ui-state-active");
-                $this.selectOption($(this));
-                if($this.optionUnselectable) {
-                    if (isActive)
-                        $this.unselectOption($(this), isActive);
+                if($(this).hasClass("ui-state-active")) {
+                    if($this.options.unselectable)
+                        $this.unselectOption(btn);
+                }
+                else {
+                    $this.unselectOption(btn.siblings('.ui-state-active'));
+                    $this.selectOption(btn);
                 }
             })
             .on('focus', function() {            
@@ -59,17 +64,17 @@
                 $(this).removeClass('ui-state-focus');
             });  
         },
+        
         selectOption: function(btn) {
             this.input.val(btn.data('value'));
-            btn.trigger('focus');
             btn.addClass('ui-state-active');
         },
+        
         unselectOption: function(btn){
             this.input.val('');
-            btn.removeClass('ui-state-active');
-            btn.trigger('blur');
-            
+            btn.removeClass('ui-state-active');            
         }  
+        
     });
     
 })();
