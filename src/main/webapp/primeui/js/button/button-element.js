7f959f6b9f1cde6da1efc7a143f822ffe8c18426
Çağatay Çivici <cagatay.civici@github.com>
2015-12-10 16:43:30 +0200

    PrimeElement for Button

---
+++ src/main/webapp/primeui/js/button/button-element.js
@@ -0,0 +1,32 @@
+xtag.register('p-button', {
+    
+    extends: 'button',
+    
+    accessors: {
+        icon: {
+            attribute: {}
+        },
+        iconPos: {
+            attribute: {}
+        }
+    },
+    
+    lifecycle: {
+        created: function () {
+            $(this).puibutton({
+                icon: this.icon,
+                iconPos: this.iconPos || 'left'
+            });
+        }
+    },
+    
+    methods: {
+        disable: function () {
+            $(this).puibutton('disable');
+        },
+        enable: function () {
+            $(this).puibutton('enable');
+        }
+    }
+    
+});
