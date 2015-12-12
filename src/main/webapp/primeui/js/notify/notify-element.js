3804c4d6d35ead7d4ffe23aaee6bba8c9aeca73f
Çağatay Çivici <cagatay.civici@github.com>
2015-12-13 00:46:09 +0200

    PrimeElement for Notify

---
+++ src/main/webapp/primeui/js/notify/notify-element.js
@@ -0,0 +1,67 @@
+if(!xtag.tags['p-notify']) {
+ 
+    xtag.register('p-notify', {
+    
+        accessors: {
+            position: {
+                attribute: {}
+            },
+            visible: {
+                attribute: {
+                    boolean: true
+                }
+            },
+            animate: {
+                attribute : {}
+            },
+            effectspeed: {
+                attribute: {}
+            },
+            easing: {
+                attribute: {}
+            },
+            onbeforehide: {
+                attribute: {}
+            },
+            onafterhide: {
+                attribute: {}
+            },
+            onbeforeshow: {
+                attribute: {}
+            },
+            onaftershow: {
+                attribute: {}
+            }
+        },
+
+        lifecycle: {
+            created: function() {
+                var $this = this;
+                this.xtag.container = $(this).append('<div></div>').children('div');
+                
+                $(this.xtag.container).puinotify({
+                    position: this.position||'top',
+                    visible: this.visible,
+                    animate: this.animate ? JSON.parse(this.animate) : true,
+                    effectspeed: this.effectspeed||'normal',
+                    easing: this.easing||'swing',
+                    beforeHide: this.onbeforehide ? function(event){PUI.executeFunctionByName($this.onbeforehide, event);} : null,
+                    afterHide: this.onafterhide ? function(event){PUI.executeFunctionByName($this.onafterhide, event);} : null,
+                    beforeShow: this.onbeforeshow ? function(event){PUI.executeFunctionByName($this.onbeforeshow, event);} : null,
+                    afterShow: this.onaftershow ? function(event){PUI.executeFunctionByName($this.onaftershow, event);} : null,
+                });
+            }
+        },
+
+        methods: {
+            show: function(content) {
+                $(this.xtag.container).puinotify('show', content);
+            },
+            hide: function(content) {
+                $(this.xtag.container).puinotify('show', content);
+            }
+        }
+        
+    });
+    
+}
