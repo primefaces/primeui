4d049d1bd37c5523af9e6f87f36d5ca940c0ee6c
Çağatay Çivici <cagatay.civici@github.com>
2015-12-15 12:07:22 +0200

    Created PrimeElement for PickList

---
+++ src/main/webapp/primeui/js/picklist/picklist-element.js
@@ -0,0 +1,78 @@
+if(!xtag.tags['p-picklist']) {
+ 
+    xtag.register('p-picklist', {
+
+        accessors: {
+            effect: {
+                attribute: {}
+            },
+            effectspeed: {
+                attribute: {}
+            },
+            sourcecaption: {
+                attribute: {}
+            },
+            targetcaption: {
+                attribute: {}
+            },
+            filter: {
+                attribute: {
+                    boolean: true
+                }
+            },
+            filterfunction: {
+                attribute: {}
+            },
+            filtermatchmode: {
+                attribute: {}
+            },
+            dragdrop: {
+                attribute: {}
+            },
+            ontransfer: {
+                attribute: {}
+            },
+            showsourcecontrols: {
+                attribute: {
+                    boolean: true
+                }
+            },
+            showtargetcontrols: {
+                attribute: {
+                    boolean: true
+                }
+            }
+        },
+
+        lifecycle: {
+            created: function() {
+                var element = $(this),
+                itemTemplate = element.children('script'),
+                $this = this;
+        
+                element.children('select').wrapAll('<div></div>');
+                this.xtag.container = element.children('div');
+                
+                $(this.xtag.container).puipicklist({
+                    effect: this.effect||'fade',
+                    effectSpeed: this.effectspeed||'fast',
+                    sourceCaption: this.sourcecaption,
+                    targetCaption: this.targetcaption,
+                    showSourceControls: this.showsourcecontrols,
+                    showTargetControls: this.showtargetcontrols,
+                    filter: this.filter,
+                    filterFunction: this.filterfunction ? PUI.resolveObjectByName(this.filterfunction): null,
+                    dragdrop: this.dragdrop ? JSON.parse(this.dragdrop) : true,
+                    template: itemTemplate.length ? itemTemplate : null,
+                    transfer: function(event, param) {PUI.executeFunctionByName($this.ontransfer, param);}
+                });
+            }
+        },
+
+        methods: {
+            
+        }
+        
+    });
+    
+}
