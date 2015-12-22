ef2d2d25ef853c782ae034c0c358232d75170731
Çağatay Çivici <cagatay.civici@github.com>
2015-12-22 15:40:07 +0200

    PrimeElement for TreeTable

--- src/main/webapp/resources/demo/js/tree-demo.js
+++ src/main/webapp/resources/demo/js/tree-demo.js
@@ -39,7 +39,7 @@ var TreeDemo = {
             }
         ],
         
-    remoteData: function(ui, response) {
+        remoteData: function(ui, response) {
             $.ajax({
                 type: "GET",
                 url: 'rest/tree/all',
@@ -49,5 +49,46 @@ var TreeDemo = {
                     response.call(this, data);
                 }
             });
-        }
+        },
+        
+        treetableData: [
+            {
+                data: {name: 'Documents', size: '75kb', type: 'Folder'},
+                children: [
+                    {
+                        data: {name: 'Work', size: '55kb', type: 'Folder'},
+                        children: [
+                            {
+                                data: {name: 'Expenses.doc', size: '30kb', type: 'Document'}
+                            },
+                            {
+                                data: {name: 'Resume.doc', size: '25kb', type: 'Resume'}
+                            }
+                        ]
+                    },
+                    {
+                        data: {name: 'Home', size: '20kb', type: 'Folder'},
+                        children: [
+                            {
+                                data: {name: 'Invoices', size: '20kb', type: 'Text'}
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                data: {name: 'Pictures', size: '150kb', type: 'Folder'},
+                children: [
+                    {
+                        data: {name: 'barcelona.jpg', size: '90kb', type: 'Picture'}
+                    },
+                    {
+                        data: {name: 'primeui.png', size: '30kb', type: 'Picture'}
+                    },
+                    {
+                        data: {name: 'optimus.jpg', size: '30kb', type: 'Picture'}
+                    }
+                ]
+            }
+        ]
 };
