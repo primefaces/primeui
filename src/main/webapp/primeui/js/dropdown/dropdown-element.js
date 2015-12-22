040b05a8e8fc52d51e6712a568f9d3702a91045c
Çağatay Çivici <cagatay.civici@github.com>
2015-12-22 16:57:51 +0200

    PrimeElement for Dropdown

---
+++ src/main/webapp/primeui/js/dropdown/dropdown-element.js
@@ -0,0 +1,72 @@
+if(!xtag.tags['p-dropdown']) {
+
+    xtag.register('p-dropdown', {
+
+        accessors: {
+            name: {
+                attribute: {}
+            },
+            effect: {
+                attribute: {}
+            },
+            effectspeed: {
+                attribute: {}
+            },
+            filter: {
+                attribute: {
+                    boolean: true
+                }
+            },
+            filtermatchmode: {
+                attribute: {}
+            },
+            casesensitivefilter: {
+                attribute: {}
+            },
+            filterfunction: {
+                attribute: {}
+            },
+            scrollheight: {
+                attribute: {}
+            },
+            appendto: {
+                attribute: {}
+            },
+            onchange: {
+                attribute: {}
+            }
+        },
+
+        lifecycle: {
+            created: function() {
+                var element = $(this),
+                $this = this;
+        
+                element.children('option').wrapAll('<select></select>');
+                this.xtag.select = element.children('select');
+                
+                if(this.name) {
+                    this.xtag.select.attr('name', this.name);
+                }
+                
+                this.xtag.select.puidropdown({
+                    effect: this.effect,
+                    effectSpeed: this.effectspeed,
+                    filter: this.filter,
+                    filterMatchMode: this.filtermatchmode,
+                    caseSensitiveFilter: this.casesensitivefilter,
+                    filterfunction: this.filterfunction,
+                    scrollHeight: this.scrollheight ? parseInt(this.scrollheight) : 200,
+                    appendTo: this.appendto||'body',
+                    change: this.onchange ? function(event) {PUI.executeFunctionByName($this.onchange, event);} : null
+                });
+            }
+        },
+
+        methods: {
+            
+        }
+        
+    });
+    
+}
