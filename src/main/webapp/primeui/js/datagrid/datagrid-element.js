37347e84b2159b36e6608289f27a1adee6a58667
Çağatay Çivici <cagatay.civici@github.com>
2016-01-10 16:11:44 +0200

    More docs and support for datasource string in data elements

--- src/main/webapp/primeui/js/datagrid/datagrid-element.js
+++ src/main/webapp/primeui/js/datagrid/datagrid-element.js
@@ -41,7 +41,7 @@ if(!xtag.tags['p-datagrid']) {
                     lazy: this.lazy,
                     columns: this.columns||3,
                     paginator: this.paginator ? {rows: this.rows ? parseInt(this.rows) : 0, totalRecords: this.totalrecords ? parseInt(this.totalrecords) : 0} : null,
-                    datasource: PUI.resolveObjectByName(this.datasource),
+                    datasource: $.type(this.datasource) === 'string' ? this.datasource: PUI.resolveObjectByName(this.datasource),
                     template: $(this).children('script')
                 });
             }
