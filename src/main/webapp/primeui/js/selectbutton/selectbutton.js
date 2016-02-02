7f8b2422c8b16b7bd07f1936b62842c5ebe5dabd
Çağatay Çivici <cagatay.civici@github.com>
2016-02-02 17:49:28 +0200

    Optimizations

--- src/main/webapp/primeui/js/selectbutton/selectbutton.js
+++ src/main/webapp/primeui/js/selectbutton/selectbutton.js
@@ -1,39 +1,55 @@
+/**
+ * PrimeUI SelectButton Widget
+ */
 (function() {
 
     $.widget("primeui.puiselectbutton", {
-       
-       options: {
+
+        options: {
+            value: null,
             choices: null,
             formfield: null,
-            unselectable: false,
             tabindex: '0',
-            multiple: false
+            multiple: false,
+            enhanced: false
         },
-        
+
         _create: function() {
-            this.element.addClass('pui-selectbutton pui-buttonset ui-widget ui-corner-all').attr('tabindex');
-            
-            //create buttons
-            if(this.options.choices) {
-                this.element.addClass('pui-buttonset-' + this.options.choices.length);
-                for(var i = 0; i < this.options.choices.length; i++) {
-                    this.element.append('<div class="pui-button ui-widget ui-state-default pui-button-text-only" tabindex="' + this.options.tabindex + '" data-value="' 
-                                        + this.options.choices[i].value + '">' +
-                                        '<span class="pui-button-text ui-c">' + 
-                                        this.options.choices[i].label + 
-                                        '</span></div>');
+            if(!this.options.enhanced) {
+                this.element.addClass('pui-selectbutton pui-buttonset ui-widget ui-corner-all').attr('tabindex');
+
+                if(this.options.choices) {
+                    this.element.addClass('pui-buttonset-' + this.options.choices.length);
+                    for(var i = 0; i < this.options.choices.length; i++) {
+                        this.element.append('<div class="pui-button ui-widget ui-state-default pui-button-text-only" tabindex="' + this.options.tabindex + '" data-value="'
+                            + this.options.choices[i].value + '">' +
+                            '<span class="pui-button-text ui-c">' +
+                            this.options.choices[i].label +
+                            '</span></div>');
+                    }
                 }
             }
-            
+            else {
+                var $this = this;
+                this.options.choices = [];
+
+                this.element.children('.pui-button').each(function() {
+                    var btn = $(this),
+                        value = btn.attr('data-value'),
+                        label = btn.children('span').text();
+                    $this.options.choices.push({'label': label, 'value': value});
+                });
+            }
+
             //cornering
             this.buttons = this.element.children('div.pui-button');
+
             this.buttons.filter(':first-child').addClass('ui-corner-left');
             this.buttons.filter(':last-child').addClass('ui-corner-right');
-            
-            //Single Select Button Or Multiple Select Button Decision
-            if(!this.options.multiple)  {         
-                this.input = $('<input type="hidden"></input>').appendTo(this.element);
-            } 
+
+            if(!this.options.multiple)  {
+                this.input = $('<input type="hidden" />').appendTo(this.element);
+            }
             else {
                 this.input = $('<select class="ui-helper-hidden-accessible" multiple></select>').appendTo(this.element);
                 for (var i = 0; i < this.options.choices.length; i++) {
@@ -42,92 +58,172 @@
                 }
                 this.selectOptions = this.input.children('option');
             }
-            
+
             if(this.options.formfield) {
                 this.input.attr('name', this.options.formfield);
             }
 
+            //preselection
+            if(this.options.value !== null && this.options.value !== undefined) {
+                this._updateSelection(this.options.value);
+            }
+
             this._bindEvents();
         },
-        
+
+        _destroy: function() {
+            this._unbindEvents();
+            if(!this.options.enhanced) {
+                this.buttons.remove();
+                this.element.removeClass('pui-selectbutton pui-buttonset ui-widget ui-corner-all').removeAttr('tabindex');
+            }
+            else {
+                this.buttons.removeClass('ui-state-focus ui-state-hover ui-state-active');
+            }
+            this.input.remove();
+        },
+
+        _triggerChangeEvent: function() {
+            var $this = this;
+
+            if(this.options.multiple) {
+                var values = [],
+                    indexes = [];
+                for(var i = 0; i < $this.buttons.length; i++) {
+                    var btn = $this.buttons.eq(i);
+                    if(btn.hasClass('ui-state-active')) {
+                        values.push(btn.data('value'));
+                        indexes.push(i);
+                    }
+                }
+
+                $this._trigger('change', event, {
+                    value: values,
+                    index: indexes
+                });
+            }
+            else {
+                for(var i = 0; i < $this.buttons.length; i++) {
+                    var btn = $this.buttons.eq(i);
+                    if(btn.hasClass('ui-state-active')) {
+                        $this._trigger('change', event, {
+                            value: btn.data('value'),
+                            index: i
+                        });
+
+                        break;
+                    }
+                }
+            }
+        },
+
         _bindEvents: function() {
             var $this = this;
-            
-            this.buttons.on('mouseover', function() {
-                var btn = $(this);
-                if(!btn.hasClass('ui-state-active')) {
-                    btn.addClass('ui-state-hover');
-                }
-            })
-            .on('mouseout', function() {
-                $(this).removeClass('ui-state-hover');
-            })
-            .on('click', function(e) {
-                var btn = $(this);
-                if($(this).hasClass("ui-state-active")) {
-                    if($this.options.unselectable) {
-                        $this.unselectOption(btn);
-                        $this._trigger('change', e);
+
+            this.buttons.on('mouseover.puiselectbutton', function() {
+                    var btn = $(this);
+                    if(!btn.hasClass('ui-state-active')) {
+                        btn.addClass('ui-state-hover');
                     }
-                }
-                else {
-                    if($this.options.multiple) {
-                        $this.selectOption(btn);
+                })
+                .on('mouseout.puiselectbutton', function() {
+                    $(this).removeClass('ui-state-hover');
+                })
+                .on('click.puiselectbutton', function(e) {
+                    var btn = $(this);
+
+                    if($(this).hasClass("ui-state-active")) {
+                        $this.unselectOption(btn);
                     }
                     else {
-                        $this.unselectOption(btn.siblings('.ui-state-active'));
-                        $this.selectOption(btn);
-                    } 
-                    
-                    $this._trigger('change', e);
-                }
-            })
-            .on('focus', function() {            
-                $(this).addClass('ui-state-focus');
-            })
-            .on('blur', function() {            
-                $(this).removeClass('ui-state-focus');
-            })
-            .on('keydown', function(e) {
-                var keyCode = $.ui.keyCode;
-                if(e.which === keyCode.ENTER) {
-                    $this.element.trigger('click');
-                    e.preventDefault();
-                }
-            })
-            .on('keydown', function(e) {
-                var keyCode = $.ui.keyCode;
-                if(e.which === keyCode.SPACE||e.which === keyCode.ENTER||e.which === keyCode.NUMPAD_ENTER) {
-                    $(this).trigger('click');
-                    e.preventDefault();
-                }
-            });
+                        if($this.options.multiple) {
+                            $this.selectOption(btn);
+                        }
+                        else {
+                            $this.unselectOption(btn.siblings('.ui-state-active'));
+                            $this.selectOption(btn);
+                        }
+                    }
+
+                    $this._triggerChangeEvent();
+                })
+                .on('focus.puiselectbutton', function() {
+                    $(this).addClass('ui-state-focus');
+                })
+                .on('blur.puiselectbutton', function() {
+                    $(this).removeClass('ui-state-focus');
+                })
+                .on('keydown.puiselectbutton', function(e) {
+                    var keyCode = $.ui.keyCode;
+                    if(e.which === keyCode.SPACE||e.which === keyCode.ENTER||e.which === keyCode.NUMPAD_ENTER) {
+                        $(this).trigger('click');
+                        e.preventDefault();
+                    }
+                });
         },
-        
+
+        _unbindEvents: function() {
+            this.buttons.off('mouseover.puiselectbutton mouseout.puiselectbutton focus.puiselectbutton blur.puiselectbutton keydown.puiselectbutton click.puiselectbutton');
+        },
+
         selectOption: function(value) {
             var btn = $.isNumeric(value) ? this.element.children('.pui-button').eq(value) : value;
-            
-            if(this.options.multiple)
+
+            if(this.options.multiple) {
                 this.selectOptions.eq(btn.index()).prop('selected',true);
+            }
             else
                 this.input.val(btn.data('value'));
-            
+
             btn.addClass('ui-state-active');
         },
-        
+
         unselectOption: function(value){
             var btn = $.isNumeric(value) ? this.element.children('.pui-button').eq(value) : value;
-            
+
             if(this.options.multiple)
                 this.selectOptions.eq(btn.index()).prop('selected',false);
             else
                 this.input.val('');
 
-            btn.removeClass('ui-state-active');   
-            btn.removeClass('ui-state-focus');         
-        }
-        
-    });
-    
-})();
+            btn.removeClass('ui-state-active');
+            btn.removeClass('ui-state-focus');
+        },
 
+        _setOption: function (key, value) {
+            if (key === 'data') {
+                this.element.empty();
+                this._bindEvents();
+            }
+            else if (key === 'value') {
+                this._updateSelection(value);
+            }
+            else {
+                $.Widget.prototype._setOption.apply(this, arguments);
+            }
+        },
+
+        _updateSelection: function(value) {
+            this.buttons.removeClass('ui-state-active');
+
+            for(var i = 0; i < this.buttons.length; i++) {
+                var button = this.buttons.eq(i),
+                    buttonValue = button.attr('data-value');
+
+                if(this.options.multiple) {
+                    if($.inArray(buttonValue, value) >= 0) {
+                        button.addClass('ui-state-active');
+                    }
+                }
+                else {
+                    if(buttonValue == value) {
+                        button.addClass('ui-state-active');
+                        break;
+                    }
+                }
+            }
+        }
+
+    });
+
+})();
