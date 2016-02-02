6ffbb6d1a8b1a66a78cb63e7702e431262ddba4b
Çağatay Çivici <cagatay.civici@github.com>
2016-02-02 15:28:12 +0200

    Fixed #163 and added enhanced option

--- src/main/webapp/primeui/js/switch/switch.js
+++ src/main/webapp/primeui/js/switch/switch.js
@@ -1,37 +1,50 @@
+/**
+ * PrimeUI Switch Widget
+ */
 (function() {
 
     $.widget("primeui.puiswitch", {
-       
-       options: {
+
+        options: {
             onLabel: 'On',
             offLabel: 'Off',
-            change: null
+            checked: false,
+            change: null,
+            enhanced: false
         },
-        
-        _create: function() {
-            this.element.wrap('<div class="pui-inputswitch ui-widget ui-widget-content ui-corner-all"></div>');
-            this.container = this.element.parent();
 
-            this.element.wrap('<div class="ui-helper-hidden-accessible"></div>');
-            this.container.prepend('<div class="pui-inputswitch-off"></div>' + 
-                                    '<div class="pui-inputswitch-on ui-state-active"></div>' + 
-                                    '<div class="pui-inputswitch-handle ui-state-default"></div>');
-            
-            this.onContainer = this.container.children('.pui-inputswitch-on');
-            this.offContainer = this.container.children('.pui-inputswitch-off');            
-            this.onContainer.append('<span>'+ this.options.onLabel +'</span>');
-            this.offContainer.append('<span>'+ this.options.offLabel +'</span>');
+        _create: function() {
+            if(!this.options.enhanced) {
+                this.element.wrap('<div class="pui-inputswitch ui-widget ui-widget-content ui-corner-all"></div>');
+                this.container = this.element.parent();
+
+                this.element.wrap('<div class="ui-helper-hidden-accessible"></div>');
+                this.container.prepend('<div class="pui-inputswitch-off"></div>' +
+                    '<div class="pui-inputswitch-on ui-state-active"></div>' +
+                    '<div class="pui-inputswitch-handle ui-state-default"></div>');
+
+                this.onContainer = this.container.children('.pui-inputswitch-on');
+                this.offContainer = this.container.children('.pui-inputswitch-off');
+                this.onContainer.append('<span>'+ this.options.onLabel +'</span>');
+                this.offContainer.append('<span>'+ this.options.offLabel +'</span>');
+            }
+            else {
+                this.container = this.element.closest('.pui-inputswitch');
+                this.onContainer = this.container.children('.pui-inputswitch-on');
+                this.offContainer = this.container.children('.pui-inputswitch-off');
+            }
+
             this.onLabel = this.onContainer.children('span');
             this.offLabel = this.offContainer.children('span');
             this.handle = this.container.children('.pui-inputswitch-handle');
 
             var	onContainerWidth = this.onContainer.width(),
-            offContainerWidth = this.offContainer.width(),
-            spanPadding	= this.offLabel.innerWidth() - this.offLabel.width(),
-            handleMargins = this.handle.outerWidth() - this.handle.innerWidth();
+                offContainerWidth = this.offContainer.width(),
+                spanPadding	= this.offLabel.innerWidth() - this.offLabel.width(),
+                handleMargins = this.handle.outerWidth() - this.handle.innerWidth();
 
             var containerWidth = (onContainerWidth > offContainerWidth) ? onContainerWidth : offContainerWidth,
-            handleWidth = containerWidth;
+                handleWidth = containerWidth;
 
             this.handle.css({'width':handleWidth});
             handleWidth = this.handle.width();
@@ -48,7 +61,8 @@
             this.offContainer.css({ width: this.container.width() - 5 });
             this.offset = this.container.width() - this.handle.outerWidth();
 
-            if(this.element.prop('checked')) {
+            //default value
+            if(this.element.prop('checked')||this.options.checked) {
                 this.handle.css({ 'left': this.offset});
                 this.onContainer.css({ 'width': this.offset});
                 this.offLabel.css({ 'margin-right': -this.offset});
@@ -62,57 +76,82 @@
                 this._bindEvents();
             }
         },
-        
+
         _bindEvents: function() {
             var $this = this;
-        
-            this.container.on('click.inputSwitch', function(e) {
+
+            this.container.on('click.puiswitch', function(e) {
                 $this.toggle();
                 $this.element.trigger('focus');
             });
 
-            this.element.on('focus.inputSwitch', function(e) {
-                $this.handle.addClass('ui-state-focus');
-            })
-            .on('blur.inputSwitch', function(e) {
-                $this.handle.removeClass('ui-state-focus');
-            })
-            .on('keydown.inputSwitch', function(e) {
-                var keyCode = $.ui.keyCode;
-                if(e.which === keyCode.SPACE) {
-                    e.preventDefault();
-                }
-            })
-            .on('keyup.inputSwitch', function(e) {
-                var keyCode = $.ui.keyCode;
-                if(e.which === keyCode.SPACE) {
-                    $this.toggle();
+            this.element.on('focus.puiswitch', function(e) {
+                    $this.handle.addClass('ui-state-focus');
+                })
+                .on('blur.puiswitch', function(e) {
+                    $this.handle.removeClass('ui-state-focus');
+                })
+                .on('keydown.puiswitch', function(e) {
+                    var keyCode = $.ui.keyCode;
+                    if(e.which === keyCode.SPACE) {
+                        e.preventDefault();
+                    }
+                })
+                .on('keyup.puiswitch', function(e) {
+                    var keyCode = $.ui.keyCode;
+                    if(e.which === keyCode.SPACE) {
+                        $this.toggle();
 
-                    e.preventDefault();
-                }
-            })
-            .on('change.inputSwitch', function(e) {
-                if($this.element.prop('checked'))
-                    $this._checkUI();
-                else
-                    $this._uncheckUI();
-                
-                $this._trigger('change', e);
-            });
+                        e.preventDefault();
+                    }
+                })
+                .on('change.puiswitch', function(e) {
+                    if($this.element.prop('checked')||$this.options.checked)
+                        $this._checkUI();
+                    else
+                        $this._uncheckUI();
+
+                    $this._trigger('change', e, {checked: $this.options.checked});
+                });
         },
-        
+
+        _unbindEvents: function() {
+            this.container.off('click.puiswitch');
+            this.element.off('focus.puiswitch blur.puiswitch keydown.puiswitch keyup.puiswitch change.puiswitch');
+        },
+
+        _destroy: function() {
+            this._unbindEvents();
+
+            if(!this.options.enhanced) {
+                this.onContainer.remove();
+                this.offContainer.remove();
+                this.handle.remove();
+                this.element.unwrap().unwrap();
+            }
+            else {
+                this.container.css('width', 'auto');
+                this.onContainer.css('width', 'auto');
+                this.onLabel.css('width', 'auto').css('margin-left', 0);
+                this.offContainer.css('width', 'auto');
+                this.offLabel.css('width', 'auto').css('margin-left', 0);
+            }
+        },
+
         toggle: function() {
-            if(this.element.prop('checked'))
+            if(this.element.prop('checked')||this.options.checked)
                 this.uncheck();
             else
                 this.check();
         },
 
         check: function() {
+            this.options.checked = true;
             this.element.prop('checked', true).trigger('change');
         },
 
         uncheck: function() {
+            this.options.checked = false;
             this.element.prop('checked', false).trigger('change');
         },
 
@@ -128,8 +167,19 @@
             this.onLabel.animate({marginLeft:-this.offset}, 200);
             this.offLabel.animate({marginRight:0}, 200);
             this.handle.animate({left:0}, 200);
-        }     
-    });
-    
-})();
+        },
 
+        _setOption: function(key, value) {
+            if(key === 'checked') {
+                if(value)
+                    this.check();
+                else
+                    this.uncheck();
+            }
+            else {
+                $.Widget.prototype._setOption.apply(this, arguments);
+            }
+        },
+    });
+
+})();
