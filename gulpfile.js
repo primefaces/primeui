5dda27f2c8612fbed90c0b48afb996e5f0ea754a
Çağatay Çivici <cagatay.civici@github.com>
2016-03-10 16:29:21 +0200

    Fixed lazy sample

--- gulpfile.js
+++ gulpfile.js
@@ -12,13 +12,13 @@ var gulp = require('gulp'),
 //Building PrimeNg Dependencies - Js
 gulp.task('build-primeng-js', function() {
     gulp.src([
-        '!components/button/button.js', '!components/checkbox/checkbox.js', '!components/*/*-element.js',
+        '!components/button/button.js', '!components/checkbox/checkbox.js', '!components/**/*-element.js',
         '!components/datagrid/datagrid.js', '!components/datascroller/datascroller.js', '!components/datatable/datatable.js',
         '!components/fieldset/fieldset.js', '!components/inputtext/inputtext.js', '!components/inputtextarea/inputtextarea.js',
         '!components/messages/messages.js', '!components/orderlist/orderlist.js', '!components/paginator/paginator.js',
         '!components/panel/panel.js', '!components/picklist/picklist.js', '!components/progressbar/progressbar.js',
         '!components/radiobutton/radiobutton.js', '!components/rating/rating.js', '!components/togglebutton/togglebutton.js',
-        '!components/selectbutton/selectbutton.js'
+        '!components/selectbutton/selectbutton.js',
         'components/*/*.js'
     ])
 	.pipe(concat('primeui-ng.js'))
