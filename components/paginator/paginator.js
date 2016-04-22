24947b1a0eb3894d2c2496073503396ca0241dbf
Mrtcndkn <mrtcndkn@gmail.com>
2016-04-22 14:29:05 +0300

    fixed #233 - Array Prototype Error

--- components/paginator/paginator.js
+++ components/paginator/paginator.js
@@ -18,26 +18,26 @@
  }(function ($) {
 
     var ElementHandlers = {
-        
+
         '{FirstPageLink}': {
             markup: '<span class="ui-paginator-first ui-paginator-element ui-state-default ui-corner-all"><span class="fa fa-step-backward"></span></span>',
-            
+
             create: function(paginator) {
                 var element = $(this.markup);
-                
+
                 if(paginator.options.page === 0) {
                     element.addClass('ui-state-disabled');
                 }
-                
+
                 element.on('click.puipaginator', function() {
                     if(!$(this).hasClass("ui-state-disabled")) {
                         paginator.option('page', 0);
                     }
                 });
-                                
+
                 return element;
             },
-            
+
             update: function(element, state) {
                 if(state.page === 0) {
                     element.addClass('ui-state-disabled').removeClass('ui-state-hover ui-state-active');
@@ -47,26 +47,26 @@
                 }
             }
         },
-                
+
         '{PreviousPageLink}': {
             markup: '<span class="ui-paginator-prev ui-paginator-element ui-state-default ui-corner-all"><span class="fa fa-backward"></span></span>',
-                    
+
             create: function(paginator) {
                 var element = $(this.markup);
-                
+
                 if(paginator.options.page === 0) {
                     element.addClass('ui-state-disabled');
                 }
-                
+
                 element.on('click.puipaginator', function() {
                     if(!$(this).hasClass("ui-state-disabled")) {
                         paginator.option('page', paginator.options.page - 1);
                     }
                 });
-                
+
                 return element;
             },
-                    
+
             update: function(element, state) {
                 if(state.page === 0) {
                     element.addClass('ui-state-disabled').removeClass('ui-state-hover ui-state-active');
@@ -76,55 +76,26 @@
                 }
             }
         },
-                
+
         '{NextPageLink}': {
             markup: '<span class="ui-paginator-next ui-paginator-element ui-state-default ui-corner-all"><span class="fa fa-forward"></span></span>',
-                    
-            create: function(paginator) {
-                var element = $(this.markup);
-                
-                if(paginator.options.page === (paginator.getPageCount() - 1)) {
-                    element.addClass('ui-state-disabled').removeClass('ui-state-hover ui-state-active');
-                }
-                
-                element.on('click.puipaginator', function() {
-                    if(!$(this).hasClass("ui-state-disabled")) {
-                        paginator.option('page', paginator.options.page + 1);
-                    }
-                });
-                
-                return element;
-            },
-                    
-            update: function(element, state) {
-                if(state.page === (state.pageCount - 1)) {
-                    element.addClass('ui-state-disabled').removeClass('ui-state-hover ui-state-active');
-                }
-                else {
-                    element.removeClass('ui-state-disabled');
-                }
-            }
-        },
-                
-        '{LastPageLink}': {
-            markup: '<span class="ui-paginator-last ui-paginator-element ui-state-default ui-corner-all"><span class="fa fa-step-forward"></span></span>',
-                    
+
             create: function(paginator) {
                 var element = $(this.markup);
 
                 if(paginator.options.page === (paginator.getPageCount() - 1)) {
                     element.addClass('ui-state-disabled').removeClass('ui-state-hover ui-state-active');
                 }
-                
+
                 element.on('click.puipaginator', function() {
                     if(!$(this).hasClass("ui-state-disabled")) {
-                        paginator.option('page', paginator.getPageCount() - 1);
+                        paginator.option('page', paginator.options.page + 1);
                     }
                 });
-                
+
                 return element;
             },
-            
+
             update: function(element, state) {
                 if(state.page === (state.pageCount - 1)) {
                     element.addClass('ui-state-disabled').removeClass('ui-state-hover ui-state-active');
@@ -134,10 +105,39 @@
                 }
             }
         },
-                
+
+        '{LastPageLink}': {
+            markup: '<span class="ui-paginator-last ui-paginator-element ui-state-default ui-corner-all"><span class="fa fa-step-forward"></span></span>',
+
+            create: function(paginator) {
+                var element = $(this.markup);
+
+                if(paginator.options.page === (paginator.getPageCount() - 1)) {
+                    element.addClass('ui-state-disabled').removeClass('ui-state-hover ui-state-active');
+                }
+
+                element.on('click.puipaginator', function() {
+                    if(!$(this).hasClass("ui-state-disabled")) {
+                        paginator.option('page', paginator.getPageCount() - 1);
+                    }
+                });
+
+                return element;
+            },
+
+            update: function(element, state) {
+                if(state.page === (state.pageCount - 1)) {
+                    element.addClass('ui-state-disabled').removeClass('ui-state-hover ui-state-active');
+                }
+                else {
+                    element.removeClass('ui-state-disabled');
+                }
+            }
+        },
+
         '{PageLinks}': {
             markup: '<span class="ui-paginator-pages"></span>',
-                    
+
             create: function(paginator) {
                 var element = $(this.markup),
                 boundaries = this.calculateBoundaries({
@@ -147,15 +147,15 @@
                 }),
                 start = boundaries[0],
                 end = boundaries[1];
-                
+
                 for(var i = start; i <= end; i++) {
                     var pageLinkNumber = (i + 1),
                     pageLinkElement = $('<span class="ui-paginator-page ui-paginator-element ui-state-default ui-corner-all">' + pageLinkNumber + "</span>");
-                    
+
                     if(i === paginator.options.page) {
                         pageLinkElement.addClass('ui-state-active');
                     }
-                    
+
                     pageLinkElement.on('click.puipaginator', function(e){
                         var link = $(this);
 
@@ -163,13 +163,13 @@
                             paginator.option('page', parseInt(link.text(), 10) - 1);
                         }
                     });
-                    
+
                     element.append(pageLinkElement);
                 }
 
                 return element;
             },
-                    
+
             update: function(element, state, paginator) {
                 var pageLinks = element.children(),
                 boundaries = this.calculateBoundaries({
@@ -179,17 +179,17 @@
                 }),
                 start = boundaries[0],
                 end = boundaries[1];
-                
+
                 pageLinks.remove();
-        
+
                 for(var i = start; i <= end; i++) {
                     var pageLinkNumber = (i + 1),
                     pageLinkElement = $('<span class="ui-paginator-page ui-paginator-element ui-state-default ui-corner-all">' + pageLinkNumber + "</span>");
-                    
+
                     if(i === state.page) {
                         pageLinkElement.addClass('ui-state-active');
                     }
-                    
+
                     pageLinkElement.on('click.puipaginator', function(e){
                         var link = $(this);
 
@@ -197,19 +197,19 @@
                             paginator.option('page', parseInt(link.text(), 10) - 1);
                         }
                     });
-                    
+
                     paginator._bindHover(pageLinkElement);
-                    
+
                     element.append(pageLinkElement);
                 }
             },
-                    
+
             calculateBoundaries: function(config) {
                 var page = config.page,
                 pageLinks = config.pageLinks,
                 pageCount = config.pageCount,
                 visiblePages = Math.min(pageLinks, pageCount);
-                
+
                 //calculate range, keep current in middle if necessary
                 var start = Math.max(0, parseInt(Math.ceil(page - ((visiblePages) / 2)), 10)),
                 end = Math.min(pageCount - 1, start + visiblePages - 1);
@@ -217,15 +217,15 @@
                 //check when approaching to last page
                 var delta = pageLinks - (end - start + 1);
                 start = Math.max(0, start - delta);
-                
+
                 return [start, end];
             }
         }
-        
+
     };
 
     $.widget("primeui.puipaginator", {
-       
+
         options: {
             pageLinks: 5,
             totalRecords: 0,
@@ -233,30 +233,30 @@
             rows: 0,
             template: '{FirstPageLink} {PreviousPageLink} {PageLinks} {NextPageLink} {LastPageLink}'
         },
-        
+
         _create: function() {
             this.element.addClass('ui-paginator ui-widget-header');
             this.paginatorElements = [];
-            
+
             var elementKeys = this.options.template.split(/[ ]+/);
             for(var i = 0; i < elementKeys.length;i++) {
                 var elementKey = elementKeys[i],
                 handler = ElementHandlers[elementKey];
-        
+
                 if(handler) {
                     var paginatorElement = handler.create(this);
                     this.paginatorElements[elementKey] = paginatorElement;
                     this.element.append(paginatorElement);
                 }
             }
-            
+
             this._bindEvents();
         },
-                
+
         _bindEvents: function() {
             this._bindHover(this.element.find('span.ui-paginator-element'));
         },
-        
+
         _bindHover: function(elements) {
             elements.on('mouseover.puipaginator', function() {
                         var el = $(this);
@@ -271,7 +271,7 @@
                         }
                     });
         },
-        
+
         _setOption: function(key, value) {
             if(key === 'page')
                 this.setPage(value);
@@ -280,7 +280,7 @@
             else
                 $.Widget.prototype._setOption.apply(this, arguments);
         },
-                
+
         setPage: function(p, silent) {
             var pc = this.getPageCount();
 
@@ -292,37 +292,39 @@
                     pageCount: pc,
                     pageLinks: this.options.pageLinks
                 };
-                
+
                 this.options.page = p;
 
                 if(!silent) {
                     this._trigger('paginate', null, newState);
                 }
-                
+
                 this.updateUI(newState);
             }
         },
-        
+
         //state contains page and totalRecords
         setState: function(state) {
             this.options.totalRecords = state.totalRecords;
             this.setPage(state.page, true);
         },
-                
+
         updateUI: function(state) {
             for(var paginatorElementKey in this.paginatorElements) {
-                ElementHandlers[paginatorElementKey].update(this.paginatorElements[paginatorElementKey], state, this);
+                if(this.paginatorElements.hasOwnProperty(paginatorElementKey)) {
+                    ElementHandlers[paginatorElementKey].update(this.paginatorElements[paginatorElementKey], state, this);
+                }
             }
         },
-                
+
         getPageCount: function() {
             return Math.ceil(this.options.totalRecords / this.options.rows)||1;
         },
-        
+
         setTotalRecords: function(value) {
             this.options.totalRecords = value;
             this.setPage(0, true);
         }
     });
-    
+
 }));
